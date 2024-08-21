8d9c98d73c68db1198c476fa12a234f5c0e6c7e0
arvee0220 <rowellverdejo@gmail.com>
2024-08-21 11:07:13 +0800

    initial draft of Hero page

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,5 +1,29 @@
+import { Moon } from "lucide-react";
+import { Button } from "../ui/button";
+
 const NavBar: React.FC = () => {
-	return <div></div>;
+	return (
+		<div className="w-11/12 h-[8rem] flex justify-between items-center">
+			<div>
+				<p className="text-sm">
+					NIGHT
+					<Moon size={15} className="inline bottom-2" />
+				</p>
+				<p>CINEMA</p>
+			</div>
+			<div className="w-6/12 flex justify-end items-center gap-5 pr-2">
+				<ul className="w-6/12 flex flex-row justify-between gap-3 p-3">
+					<li>Home</li>
+					<li>Reviews</li>
+					<li>Article</li>
+					<li>Cast</li>
+				</ul>
+				<Button variant="secondary" className="rounded-full">
+					Contact
+				</Button>
+			</div>
+		</div>
+	);
 };
 
 export default NavBar;
