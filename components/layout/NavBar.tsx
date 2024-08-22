447849614a1b25b037f8fa3b81bb352741ec2ff0
arvee0220 <rowellverdejo@gmail.com>
2024-08-22 19:40:39 +0800

    Finalize NavBar logo

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -4,12 +4,10 @@ import { Button } from "../ui/button";
 const NavBar: React.FC = () => {
 	return (
 		<div className="w-11/12 h-[8rem] flex justify-between items-center">
-			<div>
-				<p className="text-sm">
-					NIGHT
-					<Moon size={15} className="inline bottom-2" />
-				</p>
-				<p>CINEMA</p>
+			<div className="h-15">
+				<span className="text-sm">NIGHT</span>
+				<Moon size={15} className="inline relative bottom-1" />
+				<p className="font-bold text-xl relative bottom-3">CINEMA</p>
 			</div>
 			<div className="w-6/12 flex justify-end items-center gap-5 pr-2">
 				<ul className="w-6/12 flex flex-row justify-between gap-3 p-3">
