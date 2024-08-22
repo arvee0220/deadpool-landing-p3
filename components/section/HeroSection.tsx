cb7612572860a4630cc3fd148e296d8dfa821862
arvee0220 <rowellverdejo@gmail.com>
2024-08-22 21:51:58 +0800

    Add id tag

--- components/section/HeroSection.tsx
+++ components/section/HeroSection.tsx
@@ -2,7 +2,10 @@ import NavBar from "../layout/NavBar";
 
 function HeroSection() {
 	return (
-		<div className="w-screen h-screen bg-deadpool-dark bg-cover bg-center flex justify-center">
+		<div
+			className="w-screen h-screen bg-deadpool-dark bg-cover bg-center flex justify-center"
+			id="#home"
+		>
 			<NavBar />
 		</div>
 	);
