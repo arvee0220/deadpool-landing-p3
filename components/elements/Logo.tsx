70e40c69eb2f1b3d6b5ad449b9370c96b8419897
Adrian <adrian.tm@live.com>
2024-09-02 20:26:16 +0800

    fix: paddings for ArticlesToRead and Footer

--- components/elements/Logo.tsx
+++ components/elements/Logo.tsx
@@ -1,16 +1,17 @@
 import { Moon } from "lucide-react";
 
 function Logo() {
-	return (
-		<div className="h-15 px-3 md:px-1 flex flex-col text-deadpool-primary">
-			<span className="text-[16px]">
-				NIGHT <Moon size={18} className="inline relative -top-[10px] -left-[2px]" />
-			</span>
-			<span className="font-bold text-[24px] leading-none relative -top-[0.4rem]">
-				CINEMA
-			</span>
-		</div>
-	);
+  return (
+    <div className="h-15 flex flex-col text-deadpool-primary">
+      <span className="text-[16px]">
+        NIGHT{" "}
+        <Moon size={18} className="inline relative -top-[10px] -left-[2px]" />
+      </span>
+      <span className="font-bold text-[24px] leading-none relative -top-[0.4rem]">
+        CINEMA
+      </span>
+    </div>
+  );
 }
 
 export default Logo;
