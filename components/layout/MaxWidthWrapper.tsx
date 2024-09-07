60cc49c95831baa392a316c20903be586e0c949a
arvee0220 <rowellverdejo@gmail.com>
2024-09-08 02:25:27 +0800

    Fix width sizing

--- components/layout/MaxWidthWrapper.tsx
+++ components/layout/MaxWidthWrapper.tsx
@@ -2,14 +2,12 @@ import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 
 type Props = {
-  children: ReactNode;
-  className?: string;
+	children: ReactNode;
+	className?: string;
 };
 
 function MaxWidthWrapper({ children, className }: Props) {
-  return (
-    <div className={cn("max-w-[1440px] mx-auto", className)}>{children}</div>
-  );
+	return <div className={cn("max-w-screen-2xl mx-auto", className)}>{children}</div>;
 }
 
 export default MaxWidthWrapper;
